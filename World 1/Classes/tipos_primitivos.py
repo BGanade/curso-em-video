0844eda460fceea53a16873bde0d9b21752b5187
BGanade <bganade@gmail.com>
2026-02-11 16:46:14 -0300

    Create a file for the primitive data types lesson.

---
+++ World 1/Classes/tipos_primitivos.py
@@ -0,0 +1,11 @@
+n1 = input('Enter a value.: ')
+n2 = input('Enter another value : ')
+n3 = int(input('Enter the third value: '))
+n4 = int(input('Enter the last: '))
+sum1 = n1 + n2
+sum2 = n3 + n4
+print(type(n1))
+print(type(n3))
+print(sum1)
+print(sum2)
+print(f'the sum of {n3} with {n4} is {sum2}')
