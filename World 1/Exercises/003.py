cf56fb2eb8f8c4a6fea837d0d9009d2c6c62c633
BGanade <bganade@gmail.com>
2026-02-12 10:21:43 -0300

    Created Ex003 file

---
+++ World 1/Exercises/003.py
@@ -0,0 +1,4 @@
+# Create a program that reads two numbers and displays their sum.
+n1 = float(input('Enter the first number: '))
+n2 = float(input('Enter the second number: '))
+print(f'the sum of {n1} and {n2} is {n1+n2}')
